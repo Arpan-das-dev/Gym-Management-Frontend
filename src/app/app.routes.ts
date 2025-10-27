74d60f4a927ef3d2198a82b9ef36b1ca6a81af25
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-27 18:13:25 +0530

    add routes for new components eg: login, signup and add lazyloading for change, forget, reset password and email, phone verification component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,31 @@
 import { Routes } from '@angular/router';
 import { Login } from './featured/auth/login/login';
-
+import { Signup } from './featured/auth/signup/signup';
 
 export const routes: Routes = [
-    // { path: '', redirectTo: '/home', pathMatch: 'full' },
-    // { path: 'login', component: Login }
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'signup', component: Signup },
+    {
+        path: 'verify-email',
+        loadComponent: () => import('./featured/auth/email-verification/email-verification')
+            .then(m => m.EmailVerification)
+    },
+    { path: 'verify-phone',
+        loadComponent: () => import('./featured/auth/phone-verification/phone-verification')
+            .then(m => m.PhoneVerification)
+    },
+    { path: 'forgot-password',
+        loadComponent: () => import('./featured/auth/forgot-password/forgot-password')
+            .then(m => m.ForgotPassword)
+    },
+    { path: 'reset-password',
+        loadComponent: () => import('./featured/auth/reset-password/reset-password')
+            .then(m => m.ResetPassword)
+    },
+    { path : 'change-password',
+        loadComponent: () => import('./featured/auth/change-password/change-password')
+            .then(m => m.ChangePassword)
+    },
+    { path: '**', redirectTo: '/error' }
 ];
