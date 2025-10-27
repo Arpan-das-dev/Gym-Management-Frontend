7f06fc6c088fbdd8646d5f48cf707cd114821a6d
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-27 18:16:04 +0530

    add email verification component to verify account with email otp

---
+++ src/app/featured/auth/email-verification/email-verification.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Authservice } from '../../../core/services/authservice';
+import { FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-email-verification',
+  imports: [FormsModule],
+  templateUrl: './email-verification.html',
+  styleUrl: './email-verification.css',
+})
+export class EmailVerification implements OnInit{
+email: string | null = localStorage.getItem('emailForVerification');
+userName: string | null = localStorage.getItem('userName');
+otp:string = ''
+constructor(private router: Router, private authservice: Authservice){}
+sendOtpThroughEmailForUserVerification(){
+  this.authservice.sendOtpEmail(this.email || '', this.userName || '').subscribe();
+}
+resendCode(){
+  this.sendOtpThroughEmailForUserVerification();
+}
+verifyCode(){
+  this.authservice.verifyEmailOtp(this.email || '', this.otp).subscribe(
+    {
+      next: (response) => {
+        console.log(response);
+        localStorage.setItem('isEmailVerified', 'true');
+        this.router.navigate(['/verify-phone']);
+      },
+      error: (error) => {
+        alert("Invalid OTP. Please try again.");
+        console.error('OTP verification failed', error);
+      }
+    }
+  );
+}
+ngOnInit(): void {
+  this.sendOtpThroughEmailForUserVerification();
+}
+}
