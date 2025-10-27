42518292a67d50adc7282777e9f76befbbed8485
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-27 18:14:38 +0530

    add imports for router link

--- src/app/featured/auth/signup/signup.ts
+++ src/app/featured/auth/signup/signup.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Authservice } from '../../../core/services/authservice';
 import { first } from 'rxjs';
+import { signupModel } from '../../../core/Models/signupModel';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-signup',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
-export class Signup {
+export class Signup  implements OnInit {
   signupForm: FormGroup;
   constructor(private authservice: Authservice,
      private router: Router,
@@ -24,27 +26,38 @@ export class Signup {
       role:['', [Validators.required]],
     });
   }
-  const data: signupModel = {
-      firstName: this.signupForm.get('firstName')?.value,
-      lastName: this.signupForm.get('lastName')?.value,
-      email: this.signupForm.get('email')?.value,
-      phone: this.signupForm.get('phone')?.value,
-      password: this.signupForm.get('password')?.value,
-      gender: this.signupForm.get('gender')?.value,
-      role: this.signupForm.get('role')?.value,
-      joinDate: new Date()
-    };
-    bySubmit() {
+    onSubmit() {
+      const data: signupModel = {
+        firstName: this.signupForm.get('firstName')?.value,
+        lastName: this.signupForm.get('lastName')?.value,
+        email: this.signupForm.get('email')?.value,
+        phone: this.signupForm.get('phone')?.value,
+        password: this.signupForm.get('password')?.value,
+        gender: this.signupForm.get('gender')?.value,
+        role: this.signupForm.get('role')?.value,
+        joiningDate: new Date()
+      };
       if(this.signupForm.valid){
-         this.authservice.signup(this.data).pipe(first()).subscribe({
+         this.authservice.signup(data).pipe(first()).subscribe({
            next: (response) => {
              console.log('Signup successful', response);
              this.router.navigate(['/login']);
+             localStorage.setItem('emailForVerification', data.email);
+             localStorage.setItem('phoneForVerification', data.phone);
+              localStorage.setItem('userName', data.firstName + ' ' + data.lastName);
+             this.router.navigate(['/verify-email']);
            },
            error: (error) => {
+            alert("Signup failed. Please try again.");
              console.error('Signup failed', error);
            }
          });
       }
+    }
+    ngOnInit(): void {
+        this.userRole();
+    }
+    userRole():string{
+      return this.authservice.getUserRole() || '';
     }
   }
