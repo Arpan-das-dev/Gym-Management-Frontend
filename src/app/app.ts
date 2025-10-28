1bed64ccec3705853b47ef4f3854c8a53969d025
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-28 17:45:02 +0530

    updates the app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from "./shared/components/navbar/navbar";
-import { Login } from "./featured/auth/login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, Navbar],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
