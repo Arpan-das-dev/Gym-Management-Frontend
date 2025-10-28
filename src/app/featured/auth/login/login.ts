215fb3fc7dfef0962341804136394b4815961e69
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-28 17:48:29 +0530

    add router link import in the login component

--- src/app/featured/auth/login/login.ts
+++ src/app/featured/auth/login/login.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { Authservice } from '../../../core/services/authservice';
 import { loginModel } from '../../../core/Models/loginModels';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule,ReactiveFormsModule],
+  imports: [FormsModule,ReactiveFormsModule,RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
