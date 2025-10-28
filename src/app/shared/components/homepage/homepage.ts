2a954bb6dc6ab544b86a22e1ea326c1a62bfc56d
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-28 17:46:46 +0530

    add ts file for the homepage component

--- src/app/shared/components/homepage/homepage.ts
+++ src/app/shared/components/homepage/homepage.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../navbar/navbar";
+import { Router } from '@angular/router';
+import { Authservice } from '../../../core/services/authservice';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faDumbbell, faChartLine, faUsers, faIdBadge, IconDefinition, } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [Navbar,FontAwesomeModule],
+  standalone:true,
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
 export class Homepage {
-
+  icons  ={
+    dumbell: faDumbbell,
+    chart: faChartLine,
+    users: faUsers,
+    badge: faIdBadge
+  };
+  userIdentifier = localStorage.getItem('email') || localStorage.getItem('id');
+  constructor(private router: Router,
+    private authservice: Authservice,
+  ){}
+  getStarted() : void {
+    if(this.authservice.isLoggedIn()){
+      this.router.navigate(['/dashboard'],
+        {queryParams: {'user': this.userIdentifier}});
+    } else{
+      this.router.navigate(['/login']);
+    }
+  }
+  viewPlans() : void {
+    this.router.navigate(['/plans']);
+  }
 }
