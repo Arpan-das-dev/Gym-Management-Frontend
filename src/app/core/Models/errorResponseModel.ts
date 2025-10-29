69ac34be3f539e10bddc8ae063c9834f28aa60bd
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-29 21:49:13 +0530

    add error response and output model

---
+++ src/app/core/Models/errorResponseModel.ts
@@ -0,0 +1,12 @@
+export interface erroResponseModel{
+    timestamp: string;
+    status : number;
+    error: string;
+    message : string;
+    path : string;
+}
+
+export interface errorOutPutMessageModel{
+    message : string,
+    status : number
+}
