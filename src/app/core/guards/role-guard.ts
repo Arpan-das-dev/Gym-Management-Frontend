5674caf80cf6fc7bb429d4b73efb8ae1b17e6112
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-30 20:33:43 +0530

    add role guard to load dashboard per user

--- src/app/core/guards/role-guard.ts
+++ src/app/core/guards/role-guard.ts
@@ -1,5 +1,21 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { Authservice } from '../services/authservice';
 
 export const roleGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authservice = inject(Authservice);
+  const router = inject(Router)
+  if(!authservice.isLoggedIn()) {
+    router.navigate(['/login'])
+    return false;
+  }
+
+  const allowedRoles = route.data['roles'] as string[];
+  const userRole: string | null = authservice.getUserRole();
+  
+  if(allowedRoles.includes(userRole as string) && !authservice.isTokenExpired()) {
+    return true;
+  }
+   router.navigate(['/unauthorized']); // or '/login'
+  return false;
 };
