177f76162484e463249b11b3b7aa2441eded1728
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-03 20:30:13 +0530

    add auth-guard to access when only logged in

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,5 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { Authservice } from '../services/authservice';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authservice = inject(Authservice);
+  const router = inject(Router);
+  if(authservice.isLoggedIn()) {
+    return true;
+  }else{
+    router.navigate(['login'])
+    return false
+  }
 };
