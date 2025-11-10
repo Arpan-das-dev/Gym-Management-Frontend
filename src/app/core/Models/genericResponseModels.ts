ffd7966e9c75917c19cc5400cb0799ed7f16cbe2
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-10 21:42:09 +0530

    add new model for genereic repsponse

--- src/app/core/Models/genericResponseModels.ts
+++ src/app/core/Models/genericResponseModels.ts
@@ -1,6 +1,9 @@
 export interface UserCreationResponseDto {
   message: string;
 }
-export interface genericResponseMessage{
-    message: string
+export interface genericResponseMessage {
+  message?: string;
+  data?: string;
+  response?: string;
+  orderId?: string;
 }
