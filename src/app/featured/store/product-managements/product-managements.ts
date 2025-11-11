c1aca83b15d358ce8fab138d70b6e2281091d2c4
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-11 21:34:51 +0530

    add new feature for store

---
+++ src/app/featured/store/product-managements/product-managements.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-managements',
+  imports: [],
+  templateUrl: './product-managements.html',
+  styleUrl: './product-managements.css',
+})
+export class ProductManagements {
+
+}
