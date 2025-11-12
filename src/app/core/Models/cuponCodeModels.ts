8a8c355c081a8cfccd7bac4a6028ee0c95bff810
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-12 17:48:50 +0530

    add new model for cupon code management

--- src/app/core/Models/cuponCodeModels.ts
+++ src/app/core/Models/cuponCodeModels.ts
@@ -2,3 +2,8 @@ export interface CuponValidationResponse{
     valid:boolean;
     offPercentage: number;
 }
+export interface CreateCuponCodeRequestDto {
+  cuponCode: string;
+  validity: string;
+  offPercentage: number;
+}
