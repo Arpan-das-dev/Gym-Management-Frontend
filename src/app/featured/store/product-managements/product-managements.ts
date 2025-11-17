70e8aacc7f54791ad4cc2877748fa7f9b60dac7f
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-17 21:31:19 +0530

    add product management component

--- src/app/featured/store/product-managements/product-managements.ts
+++ src/app/featured/store/product-managements/product-managements.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowRight, faCogs, faHome, faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-product-managements',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './product-managements.html',
   styleUrl: './product-managements.css',
 })
 export class ProductManagements {
+icons = {
+  cogs: faCogs,
+  sparkles: faStar,
+  home: faHome,
+  arrowRight: faArrowRight
+};
 
+constructor(private router: Router){}
+returnHome() {
+  this.router.navigate(['home'])
+}
+explorePlans() {
+  this.router.navigate(['plans'])
+}
 }
