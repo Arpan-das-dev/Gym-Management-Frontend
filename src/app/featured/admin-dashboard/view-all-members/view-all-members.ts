460cec0d3c19585d2b084eda194635f93e02ec73
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-20 17:49:49 +0530

    add new component to view all members

---
+++ src/app/featured/admin-dashboard/view-all-members/view-all-members.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-members',
+  imports: [],
+  templateUrl: './view-all-members.html',
+  styleUrl: './view-all-members.css',
+})
+export class ViewAllMembers {
+
+}
