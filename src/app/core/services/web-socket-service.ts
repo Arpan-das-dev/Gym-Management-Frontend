a21ba3122846b86ecd1fafdd6cd30389f5d502bb
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-20 17:49:13 +0530

    add service to connet websocket with backend

---
+++ src/app/core/services/web-socket-service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { Client , IMessage} from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+import { Authservice } from './authservice';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebSocketService {
+
+  constructor(private auth : Authservice){}
+  private stompClient!: Client;
+  private connected = false;
+  private subscriptionQueue: { topic: string, callback: (msg: number) => void }[] = [];
+
+  
+  connect(wsUrl: string): void {
+    if (this.connected) return;
+    const token  =this.auth.getToken()
+    this.stompClient = new Client({
+      brokerURL: wsUrl, // e.g., ws://localhost:8080/ws
+      reconnectDelay: 5000,
+      // connectHeaders:{
+      //   Authorization: `Bearer ${token}`
+      // },
+      debug: () => {}
+    });
+
+    this.stompClient.onConnect = () => {
+      console.log('WebSocket connected:', wsUrl);
+      this.connected = true;
+
+      // Subscribe queued topics
+      this.subscriptionQueue.forEach(sub => {
+        this.stompClient.subscribe(sub.topic, (message: IMessage) => {
+          sub.callback(Number(message.body));
+        });
+      });
+      this.subscriptionQueue = [];
+    };
+
+    this.stompClient.onStompError = (err) => {
+      console.error('WebSocket Error:', err);
+    };
+
+    this.stompClient.activate();
+  }
+
+  subscribe(topic: string, callback: (msg: number) => void) {
+    if (this.connected) {
+      this.stompClient.subscribe(topic, (message: IMessage) => {
+        callback(Number(message.body));
+      });
+    } else {
+      // Queue until connected
+      this.subscriptionQueue.push({ topic, callback });
+    }
+  }
+
+  disconnect() {
+    if (this.stompClient) {
+      this.stompClient.deactivate();
+      this.connected = false;
+    }
+  }
+
+  ngOnDestroy() {
+    this.disconnect();
+  }
+}
+
