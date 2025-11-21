74d8ef57e09f8a4dde3bee963df07d69568ac494
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-21 23:17:58 +0530

    add new interface to define member service's method (request/response)

---
+++ src/app/core/Models/MemberServiceModels.ts
@@ -0,0 +1,33 @@
+// member mangement related models will go here
+export interface FreezeRequestDto{
+    id: string;
+    freeze: boolean;
+}
+export interface AllMemberListResponseDto {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  gender: string;
+  planExpiration: string | null; // ISO string from backend
+  frozen: boolean;
+  planDurationLeft: number;
+  planName: string;
+  planId?: string;
+  profileImageUrl: string 
+  isActive: boolean;
+}
+
+export interface AllMembersInfoWrapperResponseDtoList {
+  responseDtoList: AllMemberListResponseDto[];
+  pageNo: number;
+  pageSize: number;
+  totalElements: number;
+  totalPages: number;
+  lastPage: boolean;
+}
+export interface LoginStreakResponseDto {
+    loginStreak: number;
+    maxLoginStreak: number;
+}
