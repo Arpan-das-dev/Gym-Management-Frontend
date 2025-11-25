d9d6f4234953e69e51c17a6aa22fc6ebf3ae1592
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-25 23:16:44 +0530

    add routes for member's dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,128 +12,143 @@ import { BuyPlan } from './featured/plan/buy-plan/buy-plan';
 import { ViewAllMembers } from './featured/admin-dashboard/view-all-members/view-all-members';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: Homepage },
-    { path: 'login', component: Login },
-    { path: 'signup', component: Signup },
-    { path: 'plans', component: Plan },
-    { path: 'profile', component: ProfileCard },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Homepage },
+  { path: 'login', component: Login },
+  { path: 'signup', component: Signup },
+  { path: 'plans', component: Plan },
+  { path: 'profile', component: ProfileCard },
 
-    {
-        path: 'verify-email',
-        loadComponent: () =>
-            import('./featured/auth/email-verification/email-verification').then(
-                (m) => m.EmailVerification
-            ),
-    },
-    {
-        path: 'verify-phone',
-        loadComponent: () =>
-            import('./featured/auth/phone-verification/phone-verification').then(
-                (m) => m.PhoneVerification
-            ),
-    },
-    {
-        path: 'forgot-password',
-        loadComponent: () =>
-            import('./featured/auth/forgot-password/forgot-password').then(
-                (m) => m.ForgotPassword
-            ),
-    },
-    {
-        path: 'reset-password',
-        loadComponent: () =>
-            import('./featured/auth/reset-password/reset-password').then(
-                (m) => m.ResetPassword
-            ),
-    },
-    {
-        path: 'change-password',
-        loadComponent: () =>
-            import('./featured/auth/change-password/change-password').then(
-                (m) => m.ChangePassword
-            ),
-    },
-    {
-        path: 'dashboard',
-        canActivate: [roleGuard, authGuard],
-        data: { roles: ['ADMIN'] },
-        loadComponent: () =>
-            import('./featured/admin-dashboard/admin-dashboard').then(
-                (m) => m.AdminDashboard
-            ),
-    },
-    {
-        path: 'allMembers', canActivate: [roleGuard, authGuard],
-        data: { roles: ['ADMIN'] },
-        loadComponent: () =>
-            import('./featured/admin-dashboard/view-all-members/view-all-members').then(
-                (m) => m.ViewAllMembers
-            ),
-    },
-    {
-        path: 'createUser',
-        canActivate: [roleGuard, authGuard],
-        data: { roles: ['ADMIN'] },
-        loadComponent: () =>
-            import('./featured/auth/create-user/create-user').then(
-                (m) => m.CreateUser
-            ),
-    },
-    {
-        path: 'createPlan',
-        canActivate: [roleGuard, authGuard],
-        data: { roles: ['ADMIN'] },
-        loadComponent: () =>
-            import('./featured/plan/create-plans/create-plans').then(
-                (m) => m.CreatePlans
-            ),
-    },
-    {
-        path: 'buyPlan', canActivate: [authGuard],
-        loadComponent: () =>
-            import('./featured/plan/buy-plan/buy-plan').then(
-                m => m.BuyPlan
-            )
-    },
-    {
-        path: 'managePlans', canActivate: [roleGuard, authGuard],
-        data: { roles: ['ADMIN'] },
-        loadComponent: () =>
-            import('./featured/plan/manage-plans/manage-plans').then(
-                m => (m.ManagePlans)
-            ),
+  // path for auths
+  {
+    path: 'verify-email',
+    loadComponent: () =>
+      import('./featured/auth/email-verification/email-verification').then(
+        (m) => m.EmailVerification
+      ),
+  },
+  {
+    path: 'verify-phone',
+    loadComponent: () =>
+      import('./featured/auth/phone-verification/phone-verification').then(
+        (m) => m.PhoneVerification
+      ),
+  },
+  {
+    path: 'forgot-password',
+    loadComponent: () =>
+      import('./featured/auth/forgot-password/forgot-password').then(
+        (m) => m.ForgotPassword
+      ),
+  },
+  {
+    path: 'reset-password',
+    loadComponent: () =>
+      import('./featured/auth/reset-password/reset-password').then(
+        (m) => m.ResetPassword
+      ),
+  },
+  {
+    path: 'change-password',
+    loadComponent: () =>
+      import('./featured/auth/change-password/change-password').then(
+        (m) => m.ChangePassword
+      ),
+  },
+  // role based dashboard lazy loading with guards
+  // 1. admin dashboard
+  {
+    path: 'dashboard',
+    canActivate: [roleGuard, authGuard],
+    data: { roles: ['ADMIN'] },
+    loadComponent: () =>
+      import('./featured/admin-dashboard/admin-dashboard').then(
+        (m) => m.AdminDashboard
+      ),
+  },
 
-    },
+  // 2. member dashboard
+  {
+    path: 'member-dashboard',
+    canActivate: [roleGuard, authGuard],
+    data: { roles: ['MEMBER'] },
+    loadComponent: () =>
+      import('./featured/member-dasboard/member-dasboard').then(
+        (m) => m.MemberDasboard
+      ),
+  },
+  {
+    path: 'allMembers',
+    canActivate: [roleGuard, authGuard],
+    data: { roles: ['ADMIN'] },
+    loadComponent: () =>
+      import(
+        './featured/admin-dashboard/view-all-members/view-all-members'
+      ).then((m) => m.ViewAllMembers),
+  },
+  {
+    path: 'createUser',
+    canActivate: [roleGuard, authGuard],
+    data: { roles: ['ADMIN'] },
+    loadComponent: () =>
+      import('./featured/auth/create-user/create-user').then(
+        (m) => m.CreateUser
+      ),
+  },
+  {
+    path: 'createPlan',
+    canActivate: [roleGuard, authGuard],
+    data: { roles: ['ADMIN'] },
+    loadComponent: () =>
+      import('./featured/plan/create-plans/create-plans').then(
+        (m) => m.CreatePlans
+      ),
+  },
+  {
+    path: 'buyPlan',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./featured/plan/buy-plan/buy-plan').then((m) => m.BuyPlan),
+  },
+  {
+    path: 'managePlans',
+    canActivate: [roleGuard, authGuard],
+    data: { roles: ['ADMIN'] },
+    loadComponent: () =>
+      import('./featured/plan/manage-plans/manage-plans').then(
+        (m) => m.ManagePlans
+      ),
+  },
 
-    {
-        path: 'manageCuponCodes', canActivate: [roleGuard, authGuard],
-        data: { roles: ['ADMIN'] },
-        loadComponent: () =>
-            import('./featured/plan/cupcode-management/cupcode-management').then(
-                m => (m.CupcodeManagement)
-            ),
+  {
+    path: 'manageCuponCodes',
+    canActivate: [roleGuard, authGuard],
+    data: { roles: ['ADMIN'] },
+    loadComponent: () =>
+      import('./featured/plan/cupcode-management/cupcode-management').then(
+        (m) => m.CupcodeManagement
+      ),
+  },
 
+  {
+    path: 'manageProducts',
+    canActivate: [roleGuard, authGuard],
+    data: { roles: ['ADMIN'] },
+    loadComponent: () =>
+      import('./featured/store/product-managements/product-managements').then(
+        (m) => m.ProductManagements
+      ),
+  },
 
-    },
+  {
+    path: 'recentTransactions',
+    canActivate: [roleGuard, authGuard],
+    data: { roles: ['ADMIN'] },
+    loadComponent: () =>
+      import('./featured/plan/recent-transactions/recent-transactions').then(
+        (m) => m.RecentTransactions
+      ),
+  },
 
-    {
-        path: 'manageProducts', canActivate: [roleGuard, authGuard],
-        data: { roles: ['ADMIN'] },
-        loadComponent: () =>
-            import('./featured/store/product-managements/product-managements').then(
-                m => m.ProductManagements
-            )
-    },
-
-    {
-        path: 'recentTransactions', canActivate: [roleGuard, authGuard],
-        data: { roles: ['ADMIN'] },
-        loadComponent: () =>
-            import('./featured/plan/recent-transactions/recent-transactions').then(
-                m => m.RecentTransactions
-            )
-    },
-
-    { path: '**', redirectTo: '/error' },
+  { path: '**', redirectTo: '/error' },
 ];
