d287530abe1b4c5cc2bc91697e209629654c5818
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-25 23:22:07 +0530

    add method to fix the otp verification issue

--- src/app/featured/auth/phone-verification/phone-verification.ts
+++ src/app/featured/auth/phone-verification/phone-verification.ts
@@ -2,49 +2,125 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Authservice } from '../../../core/services/authservice';
+import { CookieService } from 'ngx-cookie-service';
+import { erroResponseModel } from '../../../core/Models/errorResponseModel';
+import { genericResponseMessage } from '../../../core/Models/genericResponseModels';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgClass } from '@angular/common';
+import { faCheckCircle, faCogs, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-phone-verification',
-  imports: [FormsModule],
+  imports: [FormsModule, FontAwesomeModule, NgClass, FormsModule],
   templateUrl: './phone-verification.html',
   styleUrl: './phone-verification.css',
 })
-export class PhoneVerification implements OnInit{
-  otp:string = '';
-  userName: string | null = localStorage.getItem('userName');
-  phone: string | null = localStorage.getItem('phoneForVerification');
-  constructor(private router : Router, private authservice: Authservice){}
+export class PhoneVerification implements OnInit {
+
+  icons = {
+    cogs: faCogs,
+    checkCircle: faCheckCircle,
+    exclamationCircle: faExclamationCircle,
+  };
+
+  loading = false;
+  showMessage = false;
+  messageText = '';
+  messageType: 'success' | 'error' = 'success';
+
+  otp: string = '';
+  userName: string | null = sessionStorage.getItem('userName');
+  phone: string | null = sessionStorage.getItem('phoneForVerification');
+
+  phonePresent: boolean = true;
+
+  constructor(
+    private router: Router,
+    private authservice: Authservice,
+    private cookie: CookieService
+  ) {}
+
   ngOnInit(): void {
-      this.sendOtpThroughPhoneForUserVerification();
+    // first try session phone, otherwise cookie phone
+    const phoneNo = this.phone || this.cookie.get('userPhone');
+
+    if (!phoneNo) {
+      this.phonePresent = false;
+      return;
+    }
+
+    this.phone = phoneNo;
+    this.sendOtpThroughPhoneForUserVerification();
   }
-resendCode(){
-  this.sendOtpThroughPhoneForUserVerification();
-}
-verifyCode(){
-  this.authservice.verifyPhoneOtp(this.phone || '', this.otp || '').subscribe(
-    response => {
-      console.log('OTP verified successfully:', response);
-         if(localStorage.getItem('isEmailVerified') === 'true'){
-        this.router.navigate(['/login']);
-    } else{
-      this.router.navigate(['/verify-email']);
+
+  showFullScreenMessage(type: 'success' | 'error', text: string) {
+    this.messageType = type;
+    this.messageText = text;
+    this.showMessage = true;
+
+    setTimeout(() => {
+      this.showMessage = false;
+    }, 3000);
+  }
+
+  resendCode() {
+    this.sendOtpThroughPhoneForUserVerification();
+  }
+
+  verifyCode() {
+
+    if (!this.otp || this.otp.length !== 6) {
+      this.showFullScreenMessage('error', 'Please enter a valid 6-digit OTP.');
+      return;
     }
-    },
-    error => {
-      console.error('Error verifying OTP:', error);
+
+    this.loading = true;
+
+    this.authservice.verifyPhoneOtp(this.phone!, this.otp).subscribe({
+      next: response => {
+        this.loading = false;
+
+        this.showFullScreenMessage('success', 'Phone verified successfully!');
+
+        // If email already verified, go login
+        if (sessionStorage.getItem('isEmailVerified') === 'true') {
+          this.router.navigate(['/login']);
+        }else if(this.authservice.isLoggedIn()) {
+          this.router.navigate(['home'])
+        } 
+        else {
+          this.router.navigate(['/verify-email']);
+        }
+      },
+      error: err => {
+        this.loading = false;
+        this.showFullScreenMessage('error', err.message || 'Invalid OTP');
+      }
+    });
+  }
+
+  sendOtpThroughPhoneForUserVerification() {
+
+    const phoneNo = this.phone || this.cookie.get('userPhone');
+    const user = this.userName || this.authservice.getUserName() || 'User';
+
+    if (!phoneNo) {
+      this.phonePresent = false;
+      return;
     }
-  );
-}
-sendOtpThroughPhoneForUserVerification() {
-  this.authservice.sendOtpEmail(this.phone || '', this.userName || '').subscribe(
-    response => {
-      console.log('OTP sent to phone:', response);
-    },
-    error => {
-      console.error('Error sending OTP to phone:', error);
-      alert('Failed to send OTP. Please try again later.');
-    }
-  );
+
+    this.loading = true;
+
+    this.authservice.sendOtpPhone(phoneNo, user).subscribe({
+      next: (res: genericResponseMessage) => {
+        this.loading = false;
+        this.showFullScreenMessage('success', res.message || "OTP sent successfully");
+      },
+      error: (err: erroResponseModel) => {
+        this.loading = false;
+        this.showFullScreenMessage('error', err.message || "Failed to send OTP");
+      }
+    });
+  }
 }
 
-}
