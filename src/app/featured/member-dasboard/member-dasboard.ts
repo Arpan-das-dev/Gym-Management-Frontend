cc8c384e2f3e8c2937831dde3a3a95c70e3e976a
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-26 23:45:13 +0530

    add methods to load all fit related data's

--- src/app/featured/member-dasboard/member-dasboard.ts
+++ src/app/featured/member-dasboard/member-dasboard.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProfileCard } from "./profile-card/profile-card";
 import { Navbar } from "../../shared/components/navbar/navbar";
+import { FitDetails } from "./fit-details/fit-details";
 
 @Component({
   selector: 'app-member-dasboard',
-  imports: [ProfileCard, Navbar],
+  imports: [ProfileCard, Navbar, FitDetails],
   templateUrl: './member-dasboard.html',
   styleUrl: './member-dasboard.css',
 })
