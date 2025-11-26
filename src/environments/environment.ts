01973e6aab970a65bda8a9c08bb6fafee52d7c4e
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-26 23:46:45 +0530

    add env files with member's url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+    apiBaseUrl : 'http://localhost:8080',
+
+    microServices : {
+        MEMBER_SERVICE : {
+            Fit : '/fitStudio/member-service/fitness'
+        }
+    }
+};
