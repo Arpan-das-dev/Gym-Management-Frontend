9a8a589b915bc7874f4ba62f1a815286e7308844
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-29 20:48:58 +0530

    add logs to debug websocket but failed

--- src/app/core/services/web-socket-service.ts
+++ src/app/core/services/web-socket-service.ts
@@ -30,10 +30,16 @@ export class WebSocketService {
       console.log('WebSocket connected:', wsUrl);
       this.connected = true;
 
+      if (!this.stompClient || !this.stompClient.active) {
+         console.warn('STOMP client is not active right after connect. Aborting subscription queue.');
+         return; // Exit if the connection is immediately lost
+      }
       // Subscribe queued topics
       this.subscriptionQueue.forEach(sub => {
         this.stompClient.subscribe(sub.topic, (message: IMessage) => {
           sub.callback(Number(message.body));
+          console.log("the websoket message is::+>", message);
+          
         });
       });
       this.subscriptionQueue = [];
