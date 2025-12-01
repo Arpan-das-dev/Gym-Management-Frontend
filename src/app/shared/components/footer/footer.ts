7819d219a580b3a27dfe1ce2749048c9b9577812
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-01 21:16:13 +0530

    add ts for @fontawesome icons import and button to scroll on top

--- src/app/shared/components/footer/footer.ts
+++ src/app/shared/components/footer/footer.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBriefcase, faEnvelope, faInfoCircle, faPhone, faQuestionCircle, faUserTie } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { RouterLink } from "@angular/router";
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
+icons = {
+  linkedin: faLinkedin,
+  briefcase: faGithub,
+  info: faInfoCircle,
+  mail: faEnvelope,
+  phone: faPhone,
+  help: faQuestionCircle,
+  trainer: faUserTie 
+};
 
 }
