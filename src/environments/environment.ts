5359a85b96819d5fef2a8d918dbc9328ac3e2d86
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-01 21:37:06 +0530

    removed environment file

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    apiBaseUrl : 'http://localhost:8080',
-
-    microServices : {
-        MEMBER_SERVICE : {
-            Fit : '/fitStudio/member-service/fitness'
-        }
-    }
-};
