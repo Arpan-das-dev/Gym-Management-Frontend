c681fdce3d98d8c3fc09669502d901de85bb4978
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-04 20:05:53 +0530

    add new service to send trainer's info without exposing the id or any credentials in public

--- src/app/core/services/data-transfer-service.ts
+++ src/app/core/services/data-transfer-service.ts
@@ -1,19 +1,40 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { publicTrainerInfoResponseDtoList } from '../Models/TrainerServiceModels';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataTransferService {
+  
   // BehaviorSubject holds the ID and ensures the receiver gets the last value.
+  private trainerObjectSource = new BehaviorSubject<publicTrainerInfoResponseDtoList| null> (null)
   private trainerIdSource = new BehaviorSubject<string | null>(null);
   
   // Expose the ID as an Observable for other components to subscribe to.
+  currentTrainerObject$ = this.trainerObjectSource.asObservable();
   currentTrainerId$ = this.trainerIdSource.asObservable();
 
   constructor() { }
 
+    // SET FULL OBJECT
+  setTrainerObject(trainer: publicTrainerInfoResponseDtoList) {
+    this.trainerObjectSource.next(trainer);
+    this.trainerIdSource.next(trainer.id);     // backup
+  }
+
+  // SET ONLY ID (fallback)
   setTrainerId(id: string) {
     this.trainerIdSource.next(id);
   }
+
+  // GETTERS (sync)
+  getTrainerObject() {
+    return this.trainerObjectSource.getValue();
+  }
+
+  getTrainerId() {
+    return this.trainerIdSource.getValue();
+  }
+
 }
