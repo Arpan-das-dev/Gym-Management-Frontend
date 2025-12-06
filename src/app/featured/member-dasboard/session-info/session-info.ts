b8e0845e256401711301117de050159c29dc25fb
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-06 21:11:22 +0530

    update

--- src/app/featured/member-dasboard/session-info/session-info.ts
+++ src/app/featured/member-dasboard/session-info/session-info.ts
@@ -8,26 +8,32 @@ import {
   AllSessionInfoResponseDto,
   SessionsResponseDto,
 } from '../../../core/Models/SessionServiceModel';
-import { faArrowDown, faArrowUp, faCalendar, faClock, faCog } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowDown,
+  faArrowUp,
+  faCalendar,
+  faClock,
+  faCog,
+} from '@fortawesome/free-solid-svg-icons';
 import { errorOutPutMessageModel } from '../../../core/Models/errorResponseModel';
 import { HttpErrorResponse } from '@angular/common/http';
 import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-session-info',
-  imports: [FontAwesomeModule,NgClass],
+  imports: [FontAwesomeModule, NgClass],
   templateUrl: './session-info.html',
   styleUrl: './session-info.css',
 })
-export class SessionInfo implements OnInit{
+export class SessionInfo implements OnInit {
   memberId: string = '';
   upcomingView: boolean = true;
   icons = {
-    calendar : faCalendar,
-arrowDown  : faArrowDown,
-arrowUp: faArrowUp,
-clock : faClock
-  }
+    calendar: faCalendar,
+    arrowDown: faArrowDown,
+    arrowUp: faArrowUp,
+    clock: faClock,
+  };
   constructor(
     private notify: NotifyService,
     private loader: LoadingService,
@@ -37,7 +43,7 @@ clock : faClock
     this.memberId = auth.getUserId();
   }
   ngOnInit(): void {
-      this.getUpcomingSessions()
+    this.getUpcomingSessions();
   }
   /**
    * here is the section for upcoming sections and we will denote the upcoming sessions as US
@@ -78,26 +84,26 @@ clock : faClock
         });
     }
   }
-  loadNextUpCommingSessions(){
-    if(this.USlastPage) {
-      this.notify.showError("Last Page Reached")
-    }else{
-       this.USPageNo++;
-    this.getUpcomingSessions()
+  loadNextUpCommingSessions() {
+    if (this.USlastPage) {
+      this.notify.showError('Last Page Reached');
+    } else {
+      this.USPageNo++;
+      this.getUpcomingSessions();
     }
   }
 
   loadPreviousUpcomingSessions() {
-    if(this.USPageNo === 0) {
-      this.notify.showError("No Previous Sessions is Present")
-    }else{
+    if (this.USPageNo === 0) {
+      this.notify.showError('No Previous Sessions is Present');
+    } else {
       this.USPageNo--;
-    this.getUpcomingSessions();
+      this.getUpcomingSessions();
     }
   }
 
   /**
-   * here is the section for past sessions and we are denoting it as 
+   * here is the section for past sessions and we are denoting it as
    * PS
    */
 
@@ -106,65 +112,77 @@ clock : faClock
   PstotalElements: number = 0;
   PstotalPages: number = 0;
   PslastPage: boolean = false;
-  PsSortDirection: string = 'DESC'
+  PsSortDirection: string = 'DESC';
   PsSessions: SessionsResponseDto[] = [];
 
   getPastSessions() {
-    this.loader.show('Fetching Past Sessions',faCog)
-    this.member.getPastSessions(this.memberId,this.PsPageNo,this.PsPageSize,this.PsSortDirection).subscribe({
+    this.loader.show('Fetching Past Sessions', faCog);
+    this.member
+      .getPastSessions(
+        this.memberId,
+        this.PsPageNo,
+        this.PsPageSize,
+        this.PsSortDirection
+      )
+      .subscribe({
         next: (res: AllSessionInfoResponseDto) => {
-            console.log('res :: \n', res);
-            this.PsSessions = res.sessionsResponseDtoList;
-            this.PsPageNo = res.pageNo;
-            this.PsPageSize = res.pageSize;
-            this.PstotalElements = res.totalElements;
-            this.PstotalPages = res.totalPages;
-            this.PslastPage = res.lastPage;
-            this.loader.hide();
-            this.notify.showSuccess('Fetched Past Sessions');
-          },
-          error: (
-            err: errorOutPutMessageModel & { err: HttpErrorResponse }
-          ) => {
-            console.log(err);
-            const message = err?.err?.message
-              ? err?.err?.message
-              : 'Failed To Load Past Sessions Due to Internal Server Error';
-            this.loader.hide();
-            this.notify.showError(message);
-          },
-    })
+          console.log('res :: \n', res);
+          this.PsSessions = res.sessionsResponseDtoList;
+          this.PsPageNo = res.pageNo;
+          this.PsPageSize = res.pageSize;
+          this.PstotalElements = res.totalElements;
+          this.PstotalPages = res.totalPages;
+          this.PslastPage = res.lastPage;
+          this.loader.hide();
+          this.notify.showSuccess('Fetched Past Sessions');
+        },
+        error: (err: errorOutPutMessageModel & { err: HttpErrorResponse }) => {
+          console.log(err);
+          const message = err?.err?.message
+            ? err?.err?.message
+            : 'Failed To Load Past Sessions Due to Internal Server Error';
+          this.loader.hide();
+          this.notify.showError(message);
+        },
+      });
   }
 
-  loadNextPastSessions(){
-if(this.PslastPage) {
-      this.notify.showError("Last Page Reached")
-    }else{
-       this.PsPageNo++;
-    this.getUpcomingSessions()
+  loadNextPastSessions() {
+    if (this.PslastPage) {
+      this.notify.showError('Last Page Reached');
+    } else {
+      this.PsPageNo++;
+      this.getUpcomingSessions();
     }
   }
-  loadPreviousPastSessions(){
- if(this.PsPageNo === 0) {
-      this.notify.showError("No Previous Sessions is Present")
-    }else{
+  loadPreviousPastSessions() {
+    if (this.PsPageNo === 0) {
+      this.notify.showError('No Previous Sessions is Present');
+    } else {
       this.PsPageNo--;
-    this.getUpcomingSessions();
+      this.getUpcomingSessions();
     }
   }
   // all helper methods
-mapDate(dateTime : string) {
-  const date = new Date(dateTime)
-  const options: Intl.DateTimeFormatOptions = {
-    weekday: 'long', // 'long' | 'short' | 'narrow'
-    day: 'numeric', // 'numeric' | '2-digit'
-    month: 'short', // 'long' | 'short' | 'narrow'
-    year: 'numeric', // 'numeric' | '2-digit'
-    hour: '2-digit', // 'numeric' | '2-digit'
-    minute: '2-digit', // 'numeric' | '2-digit'
-    hour12: false
-  };
-  const formattedString = date.toLocaleString('en-US', options);
-  return formattedString;
-}
+  mapDate(dateTime: string): string {
+    const date = new Date(dateTime);
+
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true,
+    };
+
+    // Format with locale
+    let formatted = date.toLocaleString('en-US', options);
+
+    // Remove the comma before the time → "Dec 6, 2025, 6:00 PM" → "Dec 6, 2025 6:00 PM"
+    formatted = formatted.replace(', ', ' ');
+
+    return formatted;
+  }
 }
