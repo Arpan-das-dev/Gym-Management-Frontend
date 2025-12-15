38f0357ca0c19d25777f07d5369f4d1d34ee46a9
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-15 20:13:51 +0530

    update the tailwind for popup messages

--- src/app/featured/plan/plan.ts
+++ src/app/featured/plan/plan.ts
@@ -3,10 +3,12 @@ import { Navbar } from "../../shared/components/navbar/navbar";
 import { Footer } from "../../shared/components/footer/footer";
 import { NgClass } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCancel, faCheck, faClock, faComment, faDumbbell, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faCancel, faCheck, faCheckCircle, faClock, faCog, faComment, faDumbbell, faExclamationCircle, faStar } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
 import { PlanService } from '../../core/services/plan-service';
 import { plansResponseModel } from '../../core/Models/planModel';
+import { HttpErrorResponse } from '@angular/common/http';
+import { erroResponseModel } from '../../core/Models/errorResponseModel';
 
 
 @Component({
@@ -16,13 +18,21 @@ import { plansResponseModel } from '../../core/Models/planModel';
   styleUrl: './plan.css',
 })
 export class Plan implements OnInit {
+    loading = false;
+  showMessage = false;
+  messageText = '';
+  messageType: 'success' | 'error' = 'success';
+  globalLoadinText = 'loading';
   icons = {
+    cog:faCog,
     star: faStar,
     check: faCheck,
     dumbell: faDumbbell,
     cancel: faCancel,
     clock: faClock,
-    support: faComment
+    support: faComment,
+    checkCircle: faCheckCircle,
+        exclamationCircle: faExclamationCircle,
   }
   constructor(private router: Router, private planService: PlanService) { }
 
@@ -34,11 +44,19 @@ export class Plan implements OnInit {
     this.loadAllPlans()
   }
   private loadAllPlans() {
+    this.loading = true;
+    this.globalLoadinText = "Loading Plan's Info"
     this.planService.getAllPlans().subscribe({
       next :(res) => {
         console.log("✔️ response ", res);
         this.plans = res.responseDtoList || []
         console.log('Plans:', this.plans);
+        this.loading = false;
+        this.showSuccess("Successfully Fetched all Plans")
+      }, error:(err:HttpErrorResponse) =>{
+        this.loading = false
+        console.log(err);
+        this.catchError(err,"Failed to Load All PLans Due to Internal Server Error")
       }
     })
   }
@@ -53,5 +71,32 @@ export class Plan implements OnInit {
   }
   isPopular(planId: string): boolean {
     return true;
+  }
+    catchError(err: HttpErrorResponse, defaultMsg: string) {
+    this.loading = false;
+    const errorBody: erroResponseModel | undefined = err.error;
+    let messageToShow = defaultMsg;
+    if (errorBody && errorBody.message) {
+      messageToShow = errorBody.message;
+      console.log('Backend Error Message:', messageToShow);
+    } else {
+      messageToShow = err.message || messageToShow;
+      console.error('Unknown error structure:', err);
+    }
+    this.showError(messageToShow);
+  }
+
+  showSuccess(msg: string) {
+    this.messageType = 'success';
+    this.messageText = msg;
+    this.showMessage = true;
+    setTimeout(() => (this.showMessage = false), 2500);
+  }
+
+  showError(msg: string) {
+    this.messageType = 'error';
+    this.messageText = msg;
+    this.showMessage = true;
+    setTimeout(() => (this.showMessage = false), 2500);
   }
 }
