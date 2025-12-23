c3636f39d2b45d99209bb16aae19aada6be77e7b
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-23 21:59:40 +0530

    add new component to view monthly revenue

---
+++ src/app/featured/admin-dashboard/monthly-revenue/monthly-revenue.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-monthly-revenue',
+  imports: [],
+  templateUrl: './monthly-revenue.html',
+  styleUrl: './monthly-revenue.css',
+})
+export class MonthlyRevenue {
+
+}
